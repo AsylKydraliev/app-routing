2c9efe641d732376be3edd5c8aa4e70157913aaf
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2021-12-02 18:07:40 +0600

    Созданы компоненты home, contact, my-work, about-me, resume для отображения их по ссылкам в компоненте home.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { HomeComponent } from './home/home.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    HomeComponent,
+    MyWorkComponent,
+    ContactComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule
